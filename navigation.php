51f64023837e17eb4080af74a808945cf95ba835
Cillian McNeill <cillian.mcneill2@mail.dcu.ie>
2016-12-13 19:13:11 +0000

    Initial Commit
    moving over to php and removed excess files

---
+++ navigation.php
@@ -0,0 +1,49 @@
+<!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="about.html">Products</a>
+                    </li>
+                    <li>
+                        <a href="services.html">Account </a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contact</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Basket</a>
+                    </li>
+                    <li>
+                        <div id="search_block_top">
+                                    <div id="search_block_top_inner">
+                                        <form id="searchbox" method="get" action="http://thearcheryshop.co.uk/search">
+                                            <div class="form-search">
+                                                <input type="hidden" name="controller" value="search" />
+                                                <input type="hidden" name="orderby" value="position" />
+                                                <input type="hidden" name="orderway" value="desc" />
+                                                <input class="search_query form-control" type="text" id="search_query_top" name="search_query" placeholder="Search" value="" />
+                                                <button type="submit" name="submit_search" class="btn btn-default button-search"> <i class="fa fa-search "></i><span>Search</span> </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                    </li>
+       
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
