a620703be7a48fed8365efd95c8281130f3200bb
unixbash <filip500filip@gmail.com>
2016-12-14 00:03:44 +0000

    Changes to cover image and layout

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Modern Business - Start Bootstrap Template</title>
+    <title>Shooting Style</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -20,6 +20,8 @@
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -29,42 +31,20 @@
 
 </head>
 
-<body>
+<body>  
+    <!-- 
 
-    
-
-    <!-- Header Carousel -->
     <header id="myCarousel" class="carousel slide">
-        <!-- Indicators -->
         <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
             <li data-target="#myCarousel" data-slide-to="1"></li>
             <li data-target="#myCarousel" data-slide-to="2"></li>
         </ol>
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-            <div class="item active">
-                <div class="fill" style="background-image:url('./images/cover.jpg');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 1</h2>
-                </div>
-            </div>
-            <div class="item">
-                <div class="fill" style="background-image:url('./images/cover.jpg');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 2</h2>
-                </div>
-            </div>
-            <div class="item">
-                <div class="fill" style="background-image:url('./images/cover.jpg');"></div>
-                <div class="carousel-caption">
-                    <h2>Caption 3</h2>
-                </div>
-            </div>
-        </div>
 
-        <!-- Controls -->
+        
+
+
         <a class="left carousel-control" href="#myCarousel" data-slide="prev">
             <span class="icon-prev"></span>
         </a>
@@ -72,17 +52,100 @@
             <span class="icon-next"></span>
         </a>
     </header>
-    
+    -->
+    <div class="row">
+        <div class="container-fluid"><img class="img-responsive cover-image" src="./images/cover.jpg"></div>
+    </div>
     <?php include 'navigation.php'; ?>
+<!--
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
 
-    <!-- Page Content -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img_chania.jpg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="img_chania2.jpg" alt="Chania">
+    </div>
+
+    <div class="item">
+      <img src="img_flower.jpg" alt="Flower">
+    </div>
+
+    <div class="item">
+      <img src="img_flower2.jpg" alt="Flower">
+    </div>
+  </div>
+
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+-->
+
     <div class="container">
 
         <!-- Portfolio Section -->
         <div class="row">
+            <div class="row padded15">
+                <div class="col-lg-8 col-md-12 col-sm-12">
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                        </ol>
+
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="./images/archery.jpg" alt="Chania">
+                                <div class="carousel-caption">
+                                    <h2>Caption 1</h2>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="./images/archery.jpg" alt="Chania">
+                                <div class="carousel-caption">
+                                    <h2>Caption 2</h2>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="./images/archery.jpg" alt="Chania">
+                                <div class="carousel-caption">
+                                    <h2>Caption 3</h2>
+                                </div>
+                            </div>
+
+                            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                                <span class="icon-prev"></span>
+                            </a>
+                            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                                <span class="icon-next"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-0 col-sm-0" style="background-color:#000000;height:422px">
+
+                </div>
+            </div>
             <div class="col-lg-12">
                 <h2 class="page-header">Featured Products</h2>
             </div>
+            
             <div class="col-md-4 col-sm-6">
                 <div class="item">
                     <div class="item-inner-product">
--- navigation.php
+++ navigation.php
@@ -12,7 +12,7 @@
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
+                <ul class="nav navbar-nav navbar-right nav-font">
                     <li>
                         <a href="about.html">Products</a>
                     </li>
@@ -27,18 +27,18 @@
                     </li>
                     <li>
                         <div id="search_block_top">
-                                    <div id="search_block_top_inner">
-                                        <form id="searchbox" method="get" action="./results.php">
-                                            <div class="form-search">
-                                                <input type="hidden" name="controller" value="search" />
-                                                <input type="hidden" name="orderby" value="position" />
-                                                <input type="hidden" name="orderway" value="desc" />
-                                                <input class="search_query form-control" type="text" id="search_query_top" name="search_query" placeholder="Search" value="" />
-                                                <button type="submit" name="submit_search" class="btn btn-default button-search"> <i class="fa fa-search "></i><span>Search</span> </button>
-                                            </div>
-                                        </form>
+                            <div id="search_block_top_inner">
+                                <form id="searchbox" method="get" action="./results.php">
+                                    <div class="form-search">
+                                        <input type="hidden" name="controller" value="search" />
+                                        <input type="hidden" name="orderby" value="position" />
+                                        <input type="hidden" name="orderway" value="desc" />
+                                        <input class="search_query form-control" type="text" id="search_query_top" name="search_query" placeholder="Search" value="" />
+                                        <button type="submit" name="submit_search" class="btn btn-default button-search"> <i class="fa fa-search "></i><span>Search</span> </button>
                                     </div>
-                                </div>
+                                </form>
+                            </div>
+                        </div>
                     </li>
        
                 </ul>
