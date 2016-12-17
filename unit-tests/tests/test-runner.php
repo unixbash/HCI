c97a8c0042f95fb95fbdb9adf6d147ddb342f3ef
bernardoc1001 <bernard.oconnor47@mail.dcu.ie>
2016-12-17 22:04:44 +0000

    Point the unit tests to cillians redbrick account

--- unit-tests/tests/test-runner.php
+++ unit-tests/tests/test-runner.php
@@ -13,72 +13,72 @@ class TestNavigation extends WebTestCase
 
     function checkAllNavbarLinksFromSinglePage($startPageAddress){
         #home link
-        $this->checkSingleLink($startPageAddress, 'Shooting Style Cover Image', 'http://student.computing.dcu.ie/~oconnb47/index.php');
+        $this->checkSingleLink($startPageAddress, 'Shooting Style Cover Image', 'http://www.redbrick.dcu.ie/~minisham/HCI/index.php');
 
         #products link
-        $this->checkSingleLink($startPageAddress, 'Products', 'http://student.computing.dcu.ie/~oconnb47/products.php');
+        $this->checkSingleLink($startPageAddress, 'Products', 'http://www.redbrick.dcu.ie/~minisham/HCI/products.php');
 
         #account link
-        $this->checkSingleLink($startPageAddress, 'Account', 'http://student.computing.dcu.ie/~oconnb47/account.php');
+        $this->checkSingleLink($startPageAddress, 'Account', 'http://www.redbrick.dcu.ie/~minisham/HCI/account.php');
 
         #contact link
-        $this->checkSingleLink($startPageAddress, 'Contact', 'http://student.computing.dcu.ie/~oconnb47/contact.php');
+        $this->checkSingleLink($startPageAddress, 'Contact', 'http://www.redbrick.dcu.ie/~minisham/HCI/contact.php');
 
         #basket link
-        $this->checkSingleLink($startPageAddress, 'Basket', 'http://student.computing.dcu.ie/~oconnb47/basket.php');
+        $this->checkSingleLink($startPageAddress, 'Basket', 'http://www.redbrick.dcu.ie/~minisham/HCI/basket.php');
 
     }
 
     function testAllNavbarLinksFromAllPages(){
         #index page
-        $this->checkAllNavbarLinksFromSinglePage('http://student.computing.dcu.ie/~oconnb47/index.php');
+        $this->checkAllNavbarLinksFromSinglePage('http://www.redbrick.dcu.ie/~minisham/HCI/index.php');
 
         #products page
-        $this->checkAllNavbarLinksFromSinglePage('http://student.computing.dcu.ie/~oconnb47/products.php');
+        $this->checkAllNavbarLinksFromSinglePage('http://www.redbrick.dcu.ie/~minisham/HCI/products.php');
 
         #sample product page
-        $this->checkAllNavbarLinksFromSinglePage('http://student.computing.dcu.ie/~oconnb47/sample-product.php');
+        $this->checkAllNavbarLinksFromSinglePage('http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php');
 
         #account page
-        $this->checkAllNavbarLinksFromSinglePage('http://student.computing.dcu.ie/~oconnb47/account.php');
+        $this->checkAllNavbarLinksFromSinglePage('http://www.redbrick.dcu.ie/~minisham/HCI/account.php');
 
         #contact page
-        $this->checkAllNavbarLinksFromSinglePage('http://student.computing.dcu.ie/~oconnb47/contact.php');
+        $this->checkAllNavbarLinksFromSinglePage('http://www.redbrick.dcu.ie/~minisham/HCI/contact.php');
 
         #basket page
-        $this->checkAllNavbarLinksFromSinglePage('http://student.computing.dcu.ie/~oconnb47/basket.php');
+        $this->checkAllNavbarLinksFromSinglePage('http://www.redbrick.dcu.ie/~minisham/HCI/basket.php');
 
         #404 page
-        $this->checkAllNavbarLinksFromSinglePage('http://student.computing.dcu.ie/~oconnb47/404.php');
+        $this->checkAllNavbarLinksFromSinglePage('http://www.redbrick.dcu.ie/~minisham/HCI/404.php');
 
     }
 
     function testAllBreadCrumbLinks(){
         #products page
-        $this->checkSingleLink('http://student.computing.dcu.ie/~oconnb47/products.php', 'Home', 'http://student.computing.dcu.ie/~oconnb47/index.php');
+        $this->checkSingleLink('http://www.redbrick.dcu.ie/~minisham/HCI/products.php', 'Home', 'http://www.redbrick.dcu.ie/~minisham/HCI/index.php');
 
         #account page
-        $this->checkSingleLink('http://student.computing.dcu.ie/~oconnb47/account.php', 'Home', 'http://student.computing.dcu.ie/~oconnb47/index.php');
+        $this->checkSingleLink('http://www.redbrick.dcu.ie/~minisham/HCI/account.php', 'Home', 'http://www.redbrick.dcu.ie/~minisham/HCI/index.php');
 
         #contact page
-        $this->checkSingleLink('http://student.computing.dcu.ie/~oconnb47/contact.php', 'Home', 'http://student.computing.dcu.ie/~oconnb47/index.php');
+        $this->checkSingleLink('http://www.redbrick.dcu.ie/~minisham/HCI/contact.php', 'Home', 'http://www.redbrick.dcu.ie/~minisham/HCI/index.php');
 
         #basket page
-        $this->checkSingleLink('http://student.computing.dcu.ie/~oconnb47/basket.php', 'Home', 'http://student.computing.dcu.ie/~oconnb47/index.php');
+        $this->checkSingleLink('http://www.redbrick.dcu.ie/~minisham/HCI/basket.php', 'Home', 'http://www.redbrick.dcu.ie/~minisham/HCI/index.php');
 
         #404 page
-        $this->checkSingleLink('http://student.computing.dcu.ie/~oconnb47/404.php', 'Home', 'http://student.computing.dcu.ie/~oconnb47/index.php');
+        $this->checkSingleLink('http://www.redbrick.dcu.ie/~minisham/HCI/404.php', 'Home', 'http://www.redbrick.dcu.ie/~minisham/HCI/index.php');
 
         #Sample Product Page, has two breadcrumb links
-        $this->checkSingleLink('http://student.computing.dcu.ie/~oconnb47/sample-product.php', 'Home', 'http://student.computing.dcu.ie/~oconnb47/index.php');
-        $this->checkSingleLink('http://student.computing.dcu.ie/~oconnb47/sample-product.php', 'Products', 'http://student.computing.dcu.ie/~oconnb47/products.php');
+        $this->checkSingleLink('http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php', 'Home', 'http://www.redbrick.dcu.ie/~minisham/HCI/index.php');
+        $this->checkSingleLink('http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php', 'Products', 'http://www.redbrick.dcu.ie/~minisham/HCI/products.php');
     }
 
 }
 
 class TestIndexPage extends WebTestCase {
     function testIndexPageLoad() {
-        $this->get('http://student.computing.dcu.ie/~oconnb47/index.php');
+        $this->get('http://www.redbrick.dcu.ie/~minisham/HCI/index.php');
         $this->assertResponse(200);
     }
 
@@ -98,22 +98,22 @@ class TestIndexPage extends WebTestCase {
 
     function testAllProductLinks(){
         #product 1
-        $this->checkSingleProductLinks('http://student.computing.dcu.ie/~oconnb47/index.php', 'SAS Predator Compound Bow', 'A pretty sweet bow', 'http://student.computing.dcu.ie/~oconnb47/sample-product.php', '2800', 'http://student.computing.dcu.ie/~oconnb47/basket.php');
+        $this->checkSingleProductLinks('http://www.redbrick.dcu.ie/~minisham/HCI/index.php', 'SAS Predator Compound Bow', 'A pretty sweet bow', 'http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php', '2800', 'http://www.redbrick.dcu.ie/~minisham/HCI/basket.php');
 
         #product 2
-        $this->checkSingleProductLinks('http://student.computing.dcu.ie/~oconnb47/index.php','Bear Archery Legion Compound Bow','Sneaky Bow','http://student.computing.dcu.ie/~oconnb47/sample-product.php', '2801','http://student.computing.dcu.ie/~oconnb47/basket.php');
+        $this->checkSingleProductLinks('http://www.redbrick.dcu.ie/~minisham/HCI/index.php','Bear Archery Legion Compound Bow','Sneaky Bow','http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php', '2801','http://www.redbrick.dcu.ie/~minisham/HCI/basket.php');
 
         #product 3
-        $this->checkSingleProductLinks('http://student.computing.dcu.ie/~oconnb47/index.php','Quest Radical Realtree Purple Compound Bow','Purple Lightning' , 'http://student.computing.dcu.ie/~oconnb47/sample-product.php', '2802', 'http://student.computing.dcu.ie/~oconnb47/basket.php');
+        $this->checkSingleProductLinks('http://www.redbrick.dcu.ie/~minisham/HCI/index.php','Quest Radical Realtree Purple Compound Bow','Purple Lightning' , 'http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php', '2802', 'http://www.redbrick.dcu.ie/~minisham/HCI/basket.php');
 
         #product 4
-        $this->checkSingleProductLinks('http://student.computing.dcu.ie/~oconnb47/index.php', 'Super Kodiak Wooden Bow', 'Boring bow', 'http://student.computing.dcu.ie/~oconnb47/sample-product.php', '2803', 'http://student.computing.dcu.ie/~oconnb47/basket.php');
+        $this->checkSingleProductLinks('http://www.redbrick.dcu.ie/~minisham/HCI/index.php', 'Super Kodiak Wooden Bow', 'Boring bow', 'http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php', '2803', 'http://www.redbrick.dcu.ie/~minisham/HCI/basket.php');
 
         #product 5
-        $this->checkSingleProductLinks('http://student.computing.dcu.ie/~oconnb47/index.php', 'Bear Archery Wizard Bow', 'Stick & String', 'http://student.computing.dcu.ie/~oconnb47/sample-product.php', '2804', 'http://student.computing.dcu.ie/~oconnb47/basket.php');
+        $this->checkSingleProductLinks('http://www.redbrick.dcu.ie/~minisham/HCI/index.php', 'Bear Archery Wizard Bow', 'Stick & String', 'http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php', '2804', 'http://www.redbrick.dcu.ie/~minisham/HCI/basket.php');
 
         #product 6
-        $this->checkSingleProductLinks('http://student.computing.dcu.ie/~oconnb47/index.php', 'FGM-148 Javelin automatic infrared guidance fire and forget anti-tank missile launcher', 'Boom Bow','http://student.computing.dcu.ie/~oconnb47/sample-product.php', '2805', 'http://student.computing.dcu.ie/~oconnb47/basket.php');
+        $this->checkSingleProductLinks('http://www.redbrick.dcu.ie/~minisham/HCI/index.php', 'FGM-148 Javelin automatic infrared guidance fire and forget anti-tank missile launcher', 'Boom Bow','http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php', '2805', 'http://www.redbrick.dcu.ie/~minisham/HCI/basket.php');
     }
 
 }
@@ -133,35 +133,35 @@ class TestProductsPage extends WebTestCase {
 
     function testAllProductCategoryLinks(){
         #Category 1
-        $this->checkSingleProductCategoryLinks('http://student.computing.dcu.ie/~oconnb47/products.php', 'Recurve Bow', 'Recurve Bows', 'http://student.computing.dcu.ie/~oconnb47/sample-product.php');
+        $this->checkSingleProductCategoryLinks('http://www.redbrick.dcu.ie/~minisham/HCI/products.php', 'Recurve Bow', 'Recurve Bows', 'http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php');
 
         #Category 2
-        $this->checkSingleProductCategoryLinks('http://student.computing.dcu.ie/~oconnb47/products.php', 'Arrow for archery', 'Arrows', 'http://student.computing.dcu.ie/~oconnb47/sample-product.php');
+        $this->checkSingleProductCategoryLinks('http://www.redbrick.dcu.ie/~minisham/HCI/products.php', 'Arrow for archery', 'Arrows', 'http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php');
 
         #Category 3
-        $this->checkSingleProductCategoryLinks('http://student.computing.dcu.ie/~oconnb47/products.php', 'Compound Bow', 'Compounds', 'http://student.computing.dcu.ie/~oconnb47/sample-product.php');
+        $this->checkSingleProductCategoryLinks('http://www.redbrick.dcu.ie/~minisham/HCI/products.php', 'Compound Bow', 'Compounds', 'http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php');
 
         #Category 4
-        $this->checkSingleProductCategoryLinks('http://student.computing.dcu.ie/~oconnb47/products.php', 'Target Faces', 'Target Faces', 'http://student.computing.dcu.ie/~oconnb47/sample-product.php');
+        $this->checkSingleProductCategoryLinks('http://www.redbrick.dcu.ie/~minisham/HCI/products.php', 'Target Faces', 'Target Faces', 'http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php');
 
         #Category 5
-        $this->checkSingleProductCategoryLinks('http://student.computing.dcu.ie/~oconnb47/products.php', 'Bow Accessories', 'Accessories', 'http://student.computing.dcu.ie/~oconnb47/sample-product.php');
+        $this->checkSingleProductCategoryLinks('http://www.redbrick.dcu.ie/~minisham/HCI/products.php', 'Bow Accessories', 'Accessories', 'http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php');
 
         #Category 6
-        $this->checkSingleProductCategoryLinks('http://student.computing.dcu.ie/~oconnb47/products.php', 'Recurve Limbs', 'Recurve Limbs', 'http://student.computing.dcu.ie/~oconnb47/sample-product.php');
+        $this->checkSingleProductCategoryLinks('http://www.redbrick.dcu.ie/~minisham/HCI/products.php', 'Recurve Limbs', 'Recurve Limbs', 'http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php');
     }
 }
 
 class TestSampleProductPage extends WebTestCase {
     function testIndexPageLoad() {
-        $this->get('http://student.computing.dcu.ie/~oconnb47/sample-product.php');
+        $this->get('http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php');
         $this->assertResponse(200);
     }
 
     function testAddToCartButton(){
-        $this->get('http://student.computing.dcu.ie/~oconnb47/sample-product.php');
+        $this->get('http://www.redbrick.dcu.ie/~minisham/HCI/sample-product.php');
         $this->clickLink('Add to Cart');
-        $this->assertEqual($this->getUrl(),'http://student.computing.dcu.ie/~oconnb47/basket.php');
+        $this->assertEqual($this->getUrl(),'http://www.redbrick.dcu.ie/~minisham/HCI/basket.php');
         $this->assertResponse(200);
     }
 }
